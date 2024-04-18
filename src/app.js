b41b26c8a66ea1e8a3d99bcf1553409f74b4d87c
Einar Andri <65023610+Einsitein@users.noreply.github.com>
2024-04-18 13:13:45 +0000

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import tasksRouter from './routes/tasks.js';
 import authRouter from './routes/auth.js';
@@ -8,6 +9,7 @@ import photosRouter from './routes/photos.js';
 import indexRouter from './routes/index.js';
 
 const app = express();
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
