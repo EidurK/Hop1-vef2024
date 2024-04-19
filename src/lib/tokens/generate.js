f812fd583188b4c821a123c6b6d1a57b8443fe0f
Einar Andri <65023610+Einsitein@users.noreply.github.com>
2024-04-19 11:37:57 +0000

    token 14 days

--- src/lib/tokens/generate.js
+++ src/lib/tokens/generate.js
@@ -26,7 +26,7 @@ export async function generateToken(req, res) {
     return;
   }
 
-  const token = jwt.sign({ user_id: userID }, secretKey, { expiresIn: '24h' });
+  const token = jwt.sign({ user_id: userID }, secretKey, { expiresIn: '14d' });
 
   if (!addTokenToDB(token)){
     res.status(500).json({ error: 'Database error' });
